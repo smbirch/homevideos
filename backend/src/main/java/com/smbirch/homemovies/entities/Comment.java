3bf06e2690bd6d96080383a389fb5d2317302b41
smbirch <spencer.m.birch@gmail.com>
2024-05-27 18:55:26 -0700

    adjusts postgresql data type for comment text to TEXT

--- backend/src/main/java/com/smbirch/homemovies/entities/Comment.java
+++ backend/src/main/java/com/smbirch/homemovies/entities/Comment.java
@@ -16,6 +16,7 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(columnDefinition = "TEXT")
     private String text;
 
     private String author;
