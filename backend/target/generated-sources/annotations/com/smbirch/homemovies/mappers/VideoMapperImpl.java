0ca34defba2129cc6b0a00a8f6ce470a34b5bba0
smbirch <spencer.m.birch@gmail.com>
2024-06-05 20:40:23 -0700

    adjusts docker compose

--- backend/target/generated-sources/annotations/com/smbirch/homemovies/mappers/VideoMapperImpl.java
+++ backend/target/generated-sources/annotations/com/smbirch/homemovies/mappers/VideoMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-05-27T18:58:27-0700",
-    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
+    date = "2024-06-05T20:40:04-0700",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 22 (Homebrew)"
 )
 @Component
 public class VideoMapperImpl implements VideoMapper {
