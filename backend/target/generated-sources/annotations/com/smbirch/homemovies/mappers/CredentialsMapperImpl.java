d35eb3c49d58529a075e413f53441c200bf9a1c9
smbirch <spencer.m.birch@gmail.com>
2025-01-31 22:00:45 -0700

    updates some mappers

--- backend/target/generated-sources/annotations/com/smbirch/homemovies/mappers/CredentialsMapperImpl.java
+++ backend/target/generated-sources/annotations/com/smbirch/homemovies/mappers/CredentialsMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-01-15T15:54:12-0700",
+    date = "2025-01-31T21:58:59-0700",
     comments = "version: 1.4.1.Final, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
 )
 @Component
